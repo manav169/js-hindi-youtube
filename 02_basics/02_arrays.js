eaad7f8bd14211dd1b500872a2c8253eb2c0b321
Manav Mattu <82477203+manav169@users.noreply.github.com>
2024-01-24 22:15:00 +0530

    Update amit 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -2,7 +2,7 @@ const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
 // marvel_heros.push(dc_heros)
-
+// amit chutiya hai
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
